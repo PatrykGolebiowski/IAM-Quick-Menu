6085f19e464b434cc00c6120ce09684980696bf8
GolebiowskiPatryk <patryk.golebiowski@atos.net>
2024-02-01 19:58:24 +0100

    Added route to retrevie objects from AD

--- backend/powershell/Server.ps1
+++ backend/powershell/Server.ps1
@@ -1,3 +1,43 @@
+function ConvertTo-DateTime($Object) {
+    <#
+    .SYNOPSIS
+    Converts DateTime properties of an object to formatted strings.
+
+    .DESCRIPTION
+    The ConvertTo-DateTime function iterates through all the properties of the specified object.
+    If a property is of type DateTime, it converts the DateTime value to a formatted string.
+    The format used is "dddd, MMMM dd, yyyy hh:mm:ss tt".
+
+    .PARAMETER Object
+    The object whose DateTime properties should be formatted.
+
+    .EXAMPLE
+    $user = Get-MgUser -UserId $userId -Select $userProperties
+    $formattedUser = ConvertTo-DateTime $user
+
+    This example gets a user object with the Get-MgUser cmdlet, and then formats the DateTime properties
+    of the user object using the ConvertTo-DateTime function.
+
+    .OUTPUTS
+    PSCustomObject
+    The function returns a new custom object with the formatted DateTime properties.
+    #>
+
+    $propertyValues = @{}
+    $Object.PSObject.Properties | ForEach-Object {
+        if ($_.Value -is [DateTime]) {
+            $formattedDate = $_.Value.ToString("dddd, MMMM dd, yyyy hh:mm:ss tt", [Globalization.CultureInfo]::InvariantCulture)
+            $propertyValues[$_.Name] = $formattedDate
+        }
+        else {
+            $propertyValues[$_.Name] = $_.Value
+        }
+    }
+
+    $result = [PSCustomObject]$propertyValues
+    return $result
+}
+
 Start-PodeServer -Thread 1 {
     ### Attach to port 8080 for http
     Add-PodeEndpoint -Address "localhost" -Port 8080 -Protocol Http
@@ -17,5 +57,8 @@ Start-PodeServer -Thread 1 {
         return @{User = $user }
     }
 
+
+    ### Routes
+    Use-PodeRoutes -Path ".\routes\ActiveDirectory.ps1"
 
 }
---
+++ backend/powershell/routes/ActiveDirectory.ps1
@@ -0,0 +1,113 @@
+Add-PodeRouteGroup -Path "/activedirectory" -Routes {
+
+    Add-PodeRoute -Method Get -Path "/object" -ScriptBlock {
+        $domain = $WebEvent.Query["domain"]
+        $identity = $WebEvent.Query["identity"]
+
+        if (!$identity -or !$domain) {
+            Set-PodeResponseStatus -Code 400 -Description "Invalid query" -Exception $_
+        }
+
+        $properties = @(
+            "c",
+            "canonicalName",
+            "city",
+            "cn",
+            "co",
+            "company",
+            "country",
+            "countryCode",
+            "created",
+            "createTimeStamp",
+            "department",
+            "description",
+            "directReports",
+            "displayName",
+            "distinguishedName",
+            "division",
+            "emailAddress",
+            "employeeId",
+            "employeeNumber",
+            "employeeType",
+            "enabled",
+            "extensionAttribute1",
+            "extensionAttribute2",
+            "extensionAttribute3",
+            "extensionAttribute4",
+            "extensionAttribute5",
+            "extensionAttribute6",
+            "givenName",
+            "l",
+            "legacyExchangeDn",
+            "mail",
+            "mailNickname",
+            "manager",
+            "modified",
+            "modifyTimeStamp",
+            "msdsExternalDirectoryObjectId",
+            "msdsUserAccountControlComputed",
+            "msexchExtensionAttribute16",
+            "msexchExtensionAttribute17",
+            "msexchExtensionAttribute18",
+            "msexchExtensionAttribute20",
+            "msexchExtensionCustomAttribute1",
+            "msexchExtensionCustomAttribute3",
+            "msexchExtensionCustomAttribute4",
+            "msexchExtensionCustomAttribute5",
+            "msexchRecipientDisplayType",
+            "msexchRecipientSoftDeletedStatus",
+            "msexchRecipientTypeDetails",
+            "msexchRemoteRecipientType",
+            "name",
+            "o",
+            "objectCategory",
+            "objectClass",
+            "objectGuid",
+            "office",
+            "organization",
+            "ou",
+            "passwordExpired",
+            "passwordLastSet",
+            "passwordNeverExpires",
+            "physicalDeliveryOfficeName",
+            "postalCode",
+            "primaryGroup",
+            "primaryGroupId",
+            "proxyAddresses",
+            "pwdLastSet",
+            "samAccountName",
+            "samAccountType",
+            "showInAddressBook",
+            "sn",
+            "st",
+            "state",
+            "streetAddress",
+            "surname",
+            "targetAddress",
+            "title",
+            "userAccountControl",
+            "userPrincipalName",
+            "whenChanged",
+            "whenCreated"
+        )
+        $filter = "(sAMAccountName -eq '$identity') -or (EmailAddress -eq '$identity')"
+        $object = Get-ADUser -Filter $filter -Server $domain -Properties *
+
+        if ($object) {
+            # Filter the properties directly into a PSCustomObject for clean serialization
+            $result = [PSCustomObject]@{}
+            foreach ($prop in $properties) {
+                if ($null -ne $object.$prop) {
+                    $result | Add-Member -NotePropertyName $prop -NotePropertyValue $object.$prop
+                }
+            }
+            $result = ConvertTo-DateTime -Object $result
+    
+            Write-PodeJsonResponse -Value $result
+        }
+        else {
+            Set-PodeResponseStatus -Code 404 -Description "Not found" -Exception $_
+        }
+
+    }
+}
