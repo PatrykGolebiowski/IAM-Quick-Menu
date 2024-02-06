f7a0cc6618d26ff8c06fb4e8fcacd704148e7dc3
Patryk Gołębiowski <patryk.golebiowski1@outlook.com>
2024-02-06 22:06:24 +0100

    Update Graph.ps1 - Docs and logic

--- backend/powershell/routes/Graph.ps1
+++ backend/powershell/routes/Graph.ps1
@@ -1,7 +1,37 @@
 Add-PodeRouteGroup -Path "/api/graph" -Routes {
 
-    Add-PodeRoute -Method Get -Path "/object" -ScriptBlock {
-        $identity = $WebEvent.Query["identity"]
+    New-PodeOAStringProperty -Name "id" -Description "Entra ID or UserPrincipalName" -Required | ConvertTo-PodeOAParameter -In Path | Add-PodeOAComponentParameter -Name "Id" 
+    Add-PodeOAComponentSchema -Name "EntraUserSchema" -Schema (
+        New-PodeOAObjectProperty -Properties @(
+            (New-PodeOABoolProperty -Name "accountEnabled"),
+            (New-PodeOAStringProperty -Name "createdDateTime"),
+            (New-PodeOAStringProperty -Name "displayName"),
+            (New-PodeOAStringProperty -Name "givenName"),
+            (New-PodeOAStringProperty -Name "id"),
+            (New-PodeOAStringProperty -Name "lastPasswordChangeDateTime"),
+            (New-PodeOAStringProperty -Name "mail"),
+            (New-PodeOAStringProperty -Name "mailNickname"),
+            (New-PodeOAStringProperty -Name "proxyAddresses" -Array),
+            (New-PodeOAStringProperty -Name "surname"),
+            (New-PodeOAStringProperty -Name "userPrincipalName"),
+            (New-PodeOAStringProperty -Name "userType"),
+            (New-PodeOAStringProperty -Name "onPremisesLastSyncDateTime"),
+            (New-PodeOAObjectProperty -Name "assignedLicenses" -Properties @(
+                (New-PodeOAStringProperty -Name "id"),
+                (New-PodeOAStringProperty -Name "skuId"),
+                (New-PodeOAStringProperty -Name "skuPartNumber"),
+                (New-PodeOAObjectProperty -Name "servicePlans" -Properties @(
+                    (New-PodeOAStringProperty -Name "servicePlanId"),
+                    (New-PodeOAStringProperty -Name "servicePlanName"),
+                    (New-PodeOAStringProperty -Name "provisioningStatus"),
+                    (New-PodeOAStringProperty -Name "appliesTo")
+                ))
+            ))
+        )
+    )
+
+    Add-PodeRoute -Method Get -Path "/users/:id" -ScriptBlock {
+        $identity = $WebEvent.Parameters["id"]
         
         $baseUri = "https://graph.microsoft.com/beta/users/"
         $headers = @{
@@ -32,27 +62,51 @@ Add-PodeRouteGroup -Path "/api/graph" -Routes {
         $objectLicensesUri = "$baseUri" + "$identity" + "/licenseDetails"
         
         try {
-            $objectResponse = Invoke-RestMethod -Uri $objectDetailsUri -Headers $headers
+            $objectResponse = Invoke-RestMethod -Uri $objectDetailsUri -Headers $headers -ErrorAction 'Stop'
             $objectResponse.PSObject.Properties.Remove("@odata.context")
+
+            if ($null -ne $objectResponse) {
+                try {
+                    $licenseResponse = Invoke-RestMethod -Uri $objectLicensesUri -Headers $headers
+                    $licenseResponse = $licenseResponse.value
+                    
+                    $objectResponse | Add-Member -MemberType "NoteProperty" -Name "assignedLicenses" -value $licenseResponse
+                }
+                catch {
+                    Write-Host "Error fetching license information: $_"
+                }
+            }
+            Write-PodeJsonResponse -Value $objectResponse
+        }
+        catch [System.Net.WebException] {
+            $response = $_.Exception.Response
+            $statusCode = $response.StatusCode
+
+            if ($statusCode -eq [System.Net.HttpStatusCode]::Unauthorized) {
+                Set-PodeResponseStatus -Code 401 -Description "Error: Unauthorized (401). Check your authentication headers or token." -Exception $_.Exception
+            }
+            elseif ($statusCode -eq [System.Net.HttpStatusCode]::NotFound) {
+                Set-PodeResponseStatus -Code 404 -Description "Error: Not Found (404). The requested resource does not exist." -Exception $_.Exception
+            }
+            else {
+                Write-Host "HTTP error code: $statusCode"
+            }
         }
         catch {
-            Set-PodeResponseStatus -Code 404 -Description 'Not found' -Exception $_
+            # Handle any other non-HTTP exceptions here
+            Write-Host "An unexpected error occurred: $_"
         }
 
-        try {
-            $licenseResponse = Invoke-RestMethod -Uri $objectLicensesUri -Headers $headers
-            $licenseResponse = $licenseResponse.value
-            
-            $objectResponse | Add-Member -MemberType "NoteProperty" -Name "assignedLicenses" -value $licenseResponse
-        }
-        catch {
-            continue
-        }
+    } -PassThru |
+    Set-PodeOARouteInfo -Summary "Retrieve a user object" -Tags "Graph" -PassThru |
+    Set-PodeOARequest -Parameters @(ConvertTo-PodeOAParameter -Reference "Id") -PassThru |
+    Add-PodeOAResponse -StatusCode 200 -Description "Returns a user object" -ContentSchemas @{
+        "application/json" = "EntraUserSchema"
+    } -PassThru |
+    Add-PodeOAResponse -StatusCode 401 -Description "Error: Unauthorized (401). Check your authentication headers or token." -PassThru |
+    Add-PodeOAResponse -StatusCode 404 -Description "Error: Not Found (404). The requested resource does not exist."
+    
 
-
-        Write-PodeJsonResponse -Value $objectResponse
-
-    }
 
     Add-PodeRoute -Method Post -Path "/applications" -ScriptBlock {
         $properties = $WebEvent.Data
@@ -83,6 +137,7 @@ Add-PodeRouteGroup -Path "/api/graph" -Routes {
         Write-PodeJsonResponse $response
 
     }
+
 
     Add-PodeRoute -Method Get -Path "/applications/:id" -ScriptBlock {
         $id = $WebEvent.Parameters['id']
